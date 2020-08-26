d06974d82ac4bffb35696efde29fe32c7ba05a16
Faisal Anees <faisalanees@Faisal-Aneess-MacBook-Pro-Retina.local>
2020-08-26 08:52:18 -0400

    Added loop so interface only ends when 9 is chosen

--- calculator.py
+++ calculator.py
@@ -1,11 +1,14 @@
 
 def interface():
+   
     print("My Program")
-    print("Options")
-    print("9 - Quit")
-    choice = input("Enter your choice: ")
-    if choice == '9':
-        return
+    while True:
+        
+        print("Options")
+        print("9 - Quit")
+        choice = input("Enter your choice: ")
+        if choice == '9':
+            return
     
     
     
