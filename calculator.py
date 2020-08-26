349facac04aae94400eb25cd98561e45cad26dae
Faisal Anees <faisalanees@Faisal-Aneess-MacBook-Pro-Retina.local>
2020-08-26 08:49:31 -0400

    Developed initial User interface

--- calculator.py
+++ calculator.py
@@ -1,20 +1,12 @@
 
 def interface():
-
     print("My Program")
-    while True:
+    print("Options")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice == '9':
+        return
     
-        print("Options")
-       
-        print("9-Quit")
-        
-        choice = input("Enter your choice: ")
-        
-        if choice == '9':
-            
-            return
-
+    
+    
 interface()
-
-
-
